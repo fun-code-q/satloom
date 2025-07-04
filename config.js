aba4dddaaa1e72a53d76047ea914baf7c1999560
fun-code-q <akhilsajik97@gmail.com>
2025-07-05 01:37:26 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// ========== FIREBASE CONFIG ========== //
+const firebaseConfig = {
+    apiKey: "AIzaSyDeksN3qPZCmNuoASlEqG38XVmag6ecTh8",
+    authDomain: "satloom-rtc.firebaseapp.com",
+    databaseURL: "https://satloom-rtc-default-rtdb.firebaseio.com",
+    projectId: "satloom-rtc",
+    storageBucket: "satloom-rtc.firebasestorage.app",
+    messagingSenderId: "273627860564",
+    appId: "1:273627860564:web:c326b1bb6ffcb32fb0e7c1"
+};
