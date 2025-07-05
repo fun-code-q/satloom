5a94562efe6fa1071bd86883eab275844dc6fe8b
fun-code-q <akhilsajik97@gmail.com>
2025-07-05 02:16:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,3 +8,29 @@ const firebaseConfig = {
     messagingSenderId: "273627860564",
     appId: "1:273627860564:web:c326b1bb6ffcb32fb0e7c1"
 };
+
+// Public TURN server credentials
+const turnConfig = {
+    servers: [
+        {
+            urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+            username: 'satloom',
+            credential: 'satloom123'
+        },
+        {
+            urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
+            username: 'satloom',
+            credential: 'satloom123'
+        },
+        {
+            urls: 'turn:numb.viagenie.ca:3478',
+            username: 'satloom@satloom.com',
+            credential: 'satloom123'
+        },
+        {
+            urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            username: 'satloom',
+            credential: 'satloom123'
+        }
+    ]
+};
