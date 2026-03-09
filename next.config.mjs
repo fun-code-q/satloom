3b9d74169a925b4f7ab991930eb85ec2d7c3145b
fun-code-q <fun-code-q@example.com>
2026-03-09 13:42:03 +0100

    Fix: Downgrade Next.js to ^15.1.7 and further simplify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: '/satloom',
-  images: {
-    unoptimized: true,
-  },
   devIndicators: false,
 }
 
